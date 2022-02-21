8d648651c40d94e391c6e4ba572cd95050af8ad1
Tara <tkbaral74@gmail.com>
2022-02-21 19:03:35 +0530

    My first commit

---
+++ src/main/java/com/tara/rest/SSARestController.java
@@ -0,0 +1,23 @@
+package com.tara.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tara.service.SSAService;
+
+@RestController
+public class SSARestController {
+
+	@Autowired
+	private SSAService service;
+	
+	@GetMapping("/state/{ssn}")
+	public ResponseEntity<String> stateName(@PathVariable String ssn){
+		String stateName = service.fetchStateName(ssn);
+		return new ResponseEntity<String>(stateName, HttpStatus.OK);
+	}
+}
---
+++ src/main/java/com/tara/service/SSAService.java
@@ -0,0 +1,23 @@
+package com.tara.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SSAService {
+
+	public String fetchStateName(String ssn) {
+		String stateName = null;
+		if (ssn.startsWith("4")) {
+			stateName = "New Jersey";
+		} else if (ssn.startsWith("5")) {
+			stateName = "Rhode Island";
+		} else if (ssn.startsWith("6")) {
+			stateName = "Kentucky";
+		} else if (ssn.startsWith("7")) {
+			stateName = "Ohio";
+		} else {
+			stateName = "Invalid SSN";
+		}
+		return stateName;
+	}
+}
